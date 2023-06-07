67a32f10a7249cb3844946aa31a3dc2e3021a513
Nahid2002S <viptechmaster@gmail.com>
2023-06-07 12:10:43 +0600

    user collection added

--- index.js
+++ index.js
@@ -23,12 +23,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db('assignment-12').collection('users');
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+
+        const query = {email : user.email}
+        const existingUser = await usersCollection.findOne(query);
+        if(existingUser){
+          return res.send({message : 'user already exist'})
+        }
+
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
